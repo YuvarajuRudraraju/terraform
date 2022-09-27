bd876afd33e30044ef67c349066f51784448625d
YuvarajuRudraraju <yuvaraju84@gmail.com>
2022-09-27 14:27:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+    }
+  }
+}
 provider "aws" {
   region = "us-east-1" # define region as per your account
 }
