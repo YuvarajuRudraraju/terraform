cb61419a31368f2e761c206e170ee11eeab03cd4
YuvarajuRudraraju <yuvaraju84@gmail.com>
2022-09-27 14:22:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,8 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.2.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
+  # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1" # define region as per your account
+}
 }
 
 resource "aws_s3_bucket" "yuva_bucket" {
